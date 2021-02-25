6b5ddd6378ab9fc4c26314bf827c0b8408fc9530
LukaKantaria <l_kantaria@cu.edu.ge>
2021-02-25 22:29:31 +0400

    golden spiral fibonacci

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 class Sketch {
   constructor() {
-    
+
     this.width = window.innerWidth;
     this.height = window.innerHeight;
 
@@ -16,7 +16,101 @@ class Sketch {
     
     this.app.stage.addChild(this.container);
 
-    this.render()
+    this.phi = 0.5 + Math.sqrt(5)/2;
+    console.log(this.phi)
+
+    this.add();
+    this.addLines();
+    this.render();
+
+    console.log(this.app.renderer.width)
+  }
+
+  addLines() {
+    this.ctx = new PIXI.Graphics();
+    this.ctx.lineStyle(2, 0xff0000, 1)
+
+    let lastRight = this.width;
+    let lastBottom = lastRight / this.phi;
+    let tempHorizontal, tempVertical;
+
+    this.ctx.moveTo(0, lastBottom);
+    this.ctx.lineTo(lastRight, lastBottom);
+    this.ctx.moveTo(lastRight, lastBottom);
+    this.ctx.arc(lastRight, lastBottom, lastRight, 0.5 * Math.PI, Math.PI);
+
+    let lastLeft = lastRight / this.phi;
+    this.ctx.moveTo(lastLeft, 0);
+    this.ctx.lineTo(lastLeft, lastBottom);
+    this.ctx.moveTo(lastLeft, lastBottom);
+    this.ctx.arc(lastLeft, lastBottom, lastLeft, Math.PI, 1.5 * Math.PI);
+
+    let lastTop = lastBottom / this.phi;
+    this.ctx.moveTo(lastLeft, lastTop);
+    this.ctx.lineTo(lastRight, lastTop);
+    this.ctx.moveTo(lastLeft, lastTop);
+    this.ctx.arc(lastLeft, lastTop, lastTop, 1.5 * Math.PI, 0);
+
+    lastRight = lastRight - (lastRight-lastLeft)/this.phi;
+    this.ctx.moveTo(lastRight, lastTop);
+    this.ctx.lineTo(lastRight, lastBottom);
+    this.ctx.moveTo(lastRight, lastTop);
+    this.ctx.arc(lastRight, lastTop, lastBottom - lastTop, 0, .5 * Math.PI);
+
+    tempVertical = lastBottom - (lastBottom-lastTop)/this.phi;
+    this.ctx.moveTo(lastLeft, tempVertical);
+    this.ctx.lineTo(lastRight, tempVertical);
+    this.ctx.moveTo(lastRight, tempVertical);
+    this.ctx.arc(lastRight, tempVertical, lastBottom - tempVertical, .5 * Math.PI, Math.PI);
+
+    lastBottom = tempVertical;
+    tempHorizontal = lastLeft + (lastRight-lastLeft)/this.phi;
+    this.ctx.moveTo(tempHorizontal, lastTop);
+    this.ctx.lineTo(tempHorizontal, lastBottom);
+    this.ctx.moveTo(tempHorizontal, lastBottom);
+    this.ctx.arc(tempHorizontal, lastBottom, tempHorizontal - lastLeft, Math.PI, 1.5 * Math.PI);
+
+    lastLeft = tempHorizontal;
+    tempVertical = lastTop + (lastBottom-lastTop)/this.phi;
+    this.ctx.moveTo(lastLeft, tempVertical);
+    this.ctx.lineTo(lastRight, tempVertical);
+    this.ctx.moveTo(lastLeft, tempVertical);
+    this.ctx.arc(lastLeft, tempVertical, lastRight - lastLeft, 1.5 * Math.PI, 0);
+
+    lastTop = tempVertical;
+    tempHorizontal = lastRight - (lastRight-lastLeft)/this.phi;
+    this.ctx.moveTo(tempHorizontal, lastTop);
+    this.ctx.lineTo(tempHorizontal, lastBottom);
+    this.ctx.moveTo(tempHorizontal, lastTop);
+    this.ctx.arc(tempHorizontal, lastTop, lastRight - tempHorizontal, 0, .5 * Math.PI);
+
+    lastRight = tempHorizontal;
+    tempVertical = lastBottom - (lastBottom-lastTop)/this.phi;
+    this.ctx.moveTo(lastLeft, tempVertical);
+    this.ctx.lineTo(lastRight, tempVertical);
+    this.ctx.moveTo(lastRight, tempVertical);
+    this.ctx.arc(lastRight, tempVertical, lastRight - lastLeft, .5 * Math.PI, Math.PI);
+
+    lastBottom = tempVertical;
+    tempHorizontal = lastLeft + (lastRight-lastLeft)/this.phi;
+    this.ctx.moveTo(tempHorizontal, lastTop);
+    this.ctx.lineTo(tempHorizontal, lastBottom);
+    this.ctx.moveTo(tempHorizontal, lastBottom);
+    this.ctx.arc(tempHorizontal, lastBottom, tempHorizontal - lastLeft, Math.PI, 1.5 * Math.PI);
+
+    lastLeft = tempHorizontal;
+    tempVertical = lastTop + (lastBottom - lastTop)/this.phi;
+    this.ctx.moveTo(lastRight, tempVertical);
+    this.ctx.lineTo(lastLeft, tempVertical);
+    this.ctx.moveTo(lastLeft, tempVertical);
+    this.ctx.arc(lastLeft, tempVertical, lastRight - lastLeft, 1.5 * Math.PI, 0);
+
+    this.container.addChild(this.ctx)
+
+  }
+
+  add() {
+    
   }
 
   render() {
